d2675349c9169b9a402d92d83ebbc7153707916c
justjcurtis <justjcurtis@gmail.com>
2019-07-08 00:58:52 +0100

    all settings now stored as json in 1 file

--- pyLocker.py
+++ pyLocker.py
@@ -1,11 +1,11 @@
 import argparse
 import ctypes
 import datetime
+import json
 import os
 from pathlib import Path
 import platform
 import sys
-import time
 import uuid
 import pyautogui
 
@@ -16,26 +16,40 @@ parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                     help='** = required')
 parser.add_argument('-i', "--init", action='store_true',
     help='add -i to clear old keys from system and use key from usb if one exists')
-parser.add_argument('-s', "--strong", action='store_true',
-    help='add -s to disable unlocking without key (currently disabled due to issues)')
 args = parser.parse_args()
 args.stong = False
 
 home = str(Path.home())
-UUID = uuid.uuid4()
-lockFile = Path(os.path.join(home, '.pyLock'))
-keyLocCache = Path(os.path.join(home, '.keyLoc'))
-keyLoc = None
-keyFile = None
 osname = platform.system()
-lockCommandFile = Path(os.path.join(home, '.locCom'))
-lockCommand = None
-strongAccept = Path(os.path.join(home, '.strongAccept'))
 
-if(args.strong):
-    if(os.path.exists(strongAccept)):
-        sah = open(strongAccept, 'r')
-        if(sah.read() != 'True'):
+lockFile = Path(os.path.join(home, '.pyLocker'))
+lockSettings = None
+defaultLockSettings = {
+    'uuid': str(uuid.uuid4()),
+    'keyFile': None,
+    'lockCommand': None,
+    'strongAccept': False
+}
+
+
+def getSettings():
+    if(os.path.exists(lockFile)):
+        lockFileHandle = open(lockFile, 'r')
+        settings = lockFileHandle.read()
+        return json.loads(settings)
+    else:
+        return defaultLockSettings
+
+
+def saveSettings(settings):
+    lockFileHandle = open(lockFile, 'w')
+    lockFileHandle.write(json.dumps(settings))
+
+
+def strongAcceptWarning():
+    global lockSettings
+    if(args.strong):
+        if(not lockSettings['strongAccept']):
             print('!!!WARNING!!!')
             print('strong locking is experimental at this time')
             print('using strong locking can be dangerous')
@@ -50,136 +64,112 @@ if(args.strong):
                 if(proceed != 'lock' or proceed != '"lock"'):
                     sys.exit()
                 else:
-                    sah = open(strongAccept, 'w')
-                    sah.write('True')
-                    sah.close()
-        proceed = input('Press enter to proceed with strong locking enabled or ctrl+c to exit')
+                    lockSettings['strongAccept'] = True
+                    saveSettings(lockSettings)
+            proceed = input('Press enter to proceed with strong locking enabled or ctrl+c to exit')
 
-    
+
+def setLockCommand():
+    global lockSettings
+    if(lockSettings['lockCommand'] is None):
+        print("Leave this blank to use winleft + L hotkey to lock screen")
+        lockCommand = input('Please enter command to lock screen : ')
+        if(lockCommand == ''):
+            lockSettings['lockCommand'] = 'hotkey'
+        else:
+            lockSettings['lockCommand'] = lockCommand
+
+    saveSettings(lockSettings)
+
+
+def clearOldData():
+    print('Clearing old locker data..')
+    if(os.path.exists(lockFile)):
+        os.remove(lockFile)
+    print('Done.')
+    sys.exit()
+
+
 def init():
-    global home
-    global keyFile
-    global keyLoc
-    global keyLocCache
-    global lockCommand
-    global lockCommandFile
     global lockFile
-    global osname
-    global UUID
+    global lockSettings
 
     if(args.init):
-        print('Clearing old locker data..')
-        if(os.path.exists(lockFile)):
-            os.remove(lockFile)
-        if(os.path.exists(keyLocCache)):
-            os.remove(keyLocCache)
-        if(os.path.exists(lockCommandFile)):
-            os.remove(lockCommandFile)
-        print('Done.')
-        sys.exit()
+        clearOldData()
+
+    lockSettings = getSettings()
+    saveSettings(lockSettings)
 
     if(osname.startswith('Linux')):
-        if not(os.path.exists(lockCommandFile)):
-            print("Leave this blank to use winleft + L hotkey to lock screen")
-            lockCommand = input('Please enter command to lock screen : ')
-            lockCommandFileHandle = open(lockCommandFile, 'w')
-            if(lockCommand == ''):
-                lockCommandFileHandle.write('hotkey')
-            else:
-                lockCommandFileHandle.write(lockCommand)
-        else:
-            lockCommandFileHandle = open(lockCommandFile, 'r')
-            lockCommand = lockCommandFileHandle.read()
+        setLockCommand()
 
-    if not(os.path.exists(lockFile)):
+    if (lockSettings['keyFile'] is None):
+        print('Starting initial setup')
         print('You will need to enter the path to your usb stick to be used as a physical key')
-        keyLoc = input('Path usb root : ')
-        keyFile = Path(os.path.join(keyLoc, '.pyLock'))
-        if(os.path.exists(keyFile)):
-            keyHandle = open(keyFile, 'r')
-            UUID = keyHandle.read()
-            keyLocHandle = open(keyLocCache, 'w')
-            keyLocHandle.write(str(keyFile))
-        else:
-            keyHandle = open(keyFile, 'w')
-            keyHandle.write(str(UUID))
-            keyLocHandle = open(keyLocCache, 'w')
-            keyLocHandle.write(str(keyFile))
+        gotKey = False
+        while not gotKey:
+            keyRoot = input('Path usb root : ')
+            lockSettings['keyFile'] = os.path.join(keyRoot, '.pyLock')
+            if(os.path.exists(lockSettings['keyFile'])):
+                keyHandle = open(lockSettings['keyFile'], 'r')
+                lockSettings['uuid'] = keyHandle.read()
+            else:
+                keyHandle = open(lockSettings['keyFile'], 'w')
+                keyHandle.write(lockSettings['uuid'])
 
-        lockHandle = open(lockFile, 'w')
-        lockHandle.write(str(UUID))
-    else:
-        lockHandle = open(lockFile, 'r')
-        UUID = lockHandle.read()
-        if(os.path.exists(keyLocCache)):
-            keyLocHandle = open(keyLocCache, 'r')
-            keyFile = keyLocHandle.read()
+            gotKey = os.path.exists(lockSettings['keyFile'])
 
-
-
-    if(keyFile is None):
-        gettingKey = True
-        print('key file found on machine but no key has been registered')
-
-        while gettingKey:
-            print('You will need to enter the path to your usb stick to be used as a physical key')
-            keyLoc = input('Path usb root : ')
-            keyHandle = open(keyFile, 'w')
-            keyHandle.write(str(UUID))
-            keyLocHandle = open(keyLocCache, 'w')
-            keyLocHandle.write(str(keyFile))
-            gettingKey = not (os.path.exists(keyFile))
+        saveSettings(lockSettings)
 
 
 def lock():
-
     if(osname == 'Windows'):
         ctypes.windll.user32.LockWorkStation()
     elif(osname.startswith('Linux')):
-        if(lockCommand == 'hotkey'):
+        if(lockSettings['lockCommand'] == 'hotkey'):
             pyautogui.hotkey('winleft', 'l')
         else:
-            os.system(lockCommand)
+            os.system(lockSettings['lockCommand'])
     else:
         pyautogui.hotkey('ctrlleft', 'command', 'q')
 
 
 def lockCheck():
-    print('System is now secured by usb')
-    global keyFile
-    try:
-        kh = open(keyFile, 'r')
-        secure = kh.read() == str(UUID)
-    except Exception as ex:
-        print(ex)
-        secure = False
-        lock()
-        print(f'locked @{datetime.datetime.now()}')
-
-    while True:
+    if(lockSettings['keyFile'] is not None):
+        print('System is now secured by usb')
         try:
-            if(secure and not os.path.exists(keyFile)):
-                kh.close()
-                secure = False
-                lock()
-                print(f'locked @ {datetime.datetime.now()}')
-            elif not(secure):
-                if(os.path.exists(keyFile)):
-                    kh = open(keyFile, 'r')
-                    secure = kh.read() == str(UUID)
-                    if(secure):
-                        print(f'unlocked @ {datetime.datetime.now()}')
-                    elif(args.strong):
-                        time.sleep(1)
-                        lock()
-                elif(args.strong):
-                    time.sleep(1)
-                    lock()
+            kh = open(lockSettings['keyFile'], 'r')
+            secure = kh.read() == lockSettings['uuid']
         except Exception as ex:
             print(ex)
             secure = False
             lock()
-            print(f'locked @ {datetime.datetime.now()}')
+            print(f'locked @{datetime.datetime.now()}')
+
+        while True:
+            try:
+                if(secure and not os.path.exists(lockSettings['keyFile'])):
+                    kh.close()
+                    secure = False
+                    lock()
+                    print(f'locked @ {datetime.datetime.now()}')
+                elif not(secure):
+                    if(os.path.exists(lockSettings['keyFile'])):
+                        kh = open(lockSettings['keyFile'], 'r')
+                        secure = kh.read() == lockSettings['uuid']
+                        if(secure):
+                            print(f'unlocked @ {datetime.datetime.now()}')
+            except Exception as ex:
+                print(ex)
+                secure = False
+                lock()
+                print(f'locked @ {datetime.datetime.now()}')
+    else:
+        print('No key registered')
+        print('try running pyLocker again')
+        print('if this problem continues try running pyLocker with -i to clear old data')
+        input('press enter to exit')
+        sys.exit()
 
 
 init()
