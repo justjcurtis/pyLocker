77a0874815b70a9c4b9c7f82b4572297c5703439
justjcurtis <justjcurtis@gmail.com>
2019-07-07 22:33:21 +0100

    added cl args including: -v for version info -h for help with args -i to clean old locker files -s for stronger locking

--- pyLocker.py
+++ pyLocker.py
@@ -1,9 +1,23 @@
+import argparse
+import ctypes
 import os
 from pathlib import Path
 import platform
+import sys
+import time
 import uuid
 import pyautogui
-import ctypes
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
+parser.add_argument('-v', '--version', action='version',
+                    version='%(prog)s 1.0', help="Show program's version number and exit.")
+parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                    help='** = required')
+parser.add_argument('-i', "--init", action='store_true',
+    help='add -i to clear old keys from system and use key from usb if one exists')
+parser.add_argument('-s', "--strong", action='store_true',
+    help='add -s to disable unlocking without key')
+args = parser.parse_args()
 
 home = str(Path.home())
 UUID = uuid.uuid4()
@@ -15,6 +29,7 @@ osname = platform.system()
 lockCommandFile = Path(os.path.join(home, '.locCom'))
 lockCommand = None
 
+
 def init():
     global home
     global keyFile
@@ -25,9 +40,21 @@ def init():
     global lockFile
     global osname
     global UUID
+
+    if(args.init):
+        print('Clearing old locker data..')
+        if(os.path.exists(lockFile)):
+            os.remove(lockFile)
+        if(os.path.exists(keyLocCache)):
+            os.remove(keyLocCache)
+        if(os.path.exists(lockCommandFile)):
+            os.remove(lockCommandFile)
+        print('Done.')
+        sys.exit()
+
     if(osname.startswith('Linux')):
         if not(os.path.exists(lockCommandFile)):
-            print("leave this blank to use winleft + L hotkey to lock screen")
+            print("Leave this blank to use winleft + L hotkey to lock screen")
             lockCommand = input('Please enter command to lock screen : ')
             lockCommandFileHandle = open(lockCommandFile, 'w')
             if(lockCommand == ''):
@@ -66,13 +93,20 @@ def init():
 
     if(keyFile is None):
         gettingKey = True
+        print('key file found on machine but no key has been registered')
 
         while gettingKey:
-            keyFile = input('Path to key file : ')
+            print('You will need to enter the path to your usb stick to be used as a physical key')
+            keyLoc = input('Path usb root : ')
+            keyHandle = open(keyFile, 'w')
+            keyHandle.write(str(UUID))
+            keyLocHandle = open(keyLocCache, 'w')
+            keyLocHandle.write(str(keyFile))
             gettingKey = not (os.path.exists(keyFile))
 
 
 def lock():
+
     if(osname == 'Windows'):
         ctypes.windll.user32.LockWorkStation()
     elif(osname.startswith('Linux')):
@@ -84,23 +118,41 @@ def lock():
         pyautogui.hotkey('ctrlleft', 'command', 'q')
 
 
-
 def lockCheck():
     global keyFile
-    kh = open(keyFile, 'r')
-    secure = kh.read() == str(UUID)
+    try:
+        kh = open(keyFile, 'r')
+        secure = kh.read() == str(UUID)
+    except Exception as ex:
+        print(ex)
+        secure = False
+        lock()
+        print('lock')
+
     while True:
-        if(secure and not os.path.exists(keyFile)):
-            kh.close()
+        try:
+            if(secure and not os.path.exists(keyFile)):
+                kh.close()
+                secure = False
+                lock()
+                print('lock')
+            elif not(secure):
+                if(os.path.exists(keyFile)):
+                    kh = open(keyFile, 'r')
+                    secure = kh.read() == str(UUID)
+                    if(secure):
+                        print('unlock')
+                    elif(args.strong):
+                        time.sleep(1)
+                        lock()
+                elif(args.strong):
+                    time.sleep(1)
+                    lock()
+        except Exception as ex:
+            print(ex)
             secure = False
-            print('lock')
             lock()
-        elif not(secure):
-            if(os.path.exists(keyFile)):
-                kh = open(keyFile, 'r')
-                secure = kh.read() == str(UUID)
-                if(secure):
-                    print('unlock')
+            print('lock')
 
 
 init()
